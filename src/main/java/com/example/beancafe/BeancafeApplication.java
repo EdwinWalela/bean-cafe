39ed58b0c031850daed3ac58da8b7442a35794d2
edwinwalela <edwinwalela@gmail.com>
2022-06-18 17:29:08 +0300

    project setup

---
+++ src/main/java/com/example/beancafe/BeancafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.beancafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeancafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeancafeApplication.class, args);
+	}
+
+}
