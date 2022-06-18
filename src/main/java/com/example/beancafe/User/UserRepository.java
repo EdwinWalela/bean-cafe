ee7629778b13786ad7021b1ff1bd12d50e70f229
edwinwalela <edwinwalela@gmail.com>
2022-06-18 17:58:39 +0300

    user repository

--- src/main/java/com/example/beancafe/User/UserRepository.java
+++ src/main/java/com/example/beancafe/User/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.beancafe.User;
-public class UserRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository()
+public interface UserRepository extends JpaRepository<User,Long>{
   
 }
