1e88ca3cadd8f3c6661cb7463935f577f4b75250
edwinwalela <edwinwalela@gmail.com>
2022-06-18 18:42:21 +0300

    intergrate user service in controller

--- src/main/java/com/example/beancafe/User/UserController.java
+++ src/main/java/com/example/beancafe/User/UserController.java
@@ -1,5 +1,8 @@
 package com.example.beancafe.User;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,35 +10,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 @RequestMapping(path = "/api/v1/users")
 public class UserController {
 
-	@GetMapping()
-	public void getUsers(){
+	private final UserService userService;
 
+	@Autowired
+	public UserController(UserService userService){
+		this.userService = userService;
+	}
+
+	@GetMapping()
+	public List<User> getUsers(){
+		return this.userService.getUsers();
 	}
 	@GetMapping(path = "/{id}")
-	public void getUser(@PathVariable("id") Long id){
-
+	public User getUser(@PathVariable("id") Long id){
+		return this.userService.getUser(id);
 	}
 
 	@PostMapping()
-	public void createUser(@RequestBody User user){
-
+	public User createUser(@RequestBody User user){
+		return this.userService.createUser(user); 
 	}
 
 	@PutMapping(path = "/{id}")
-	public void updateUser(@RequestBody User user, @PathVariable("id") Long id){
-
+	public String updateUser(@RequestBody User user, @PathVariable("id") Long id){
+		this.userService.updateUser(user, id);
+		return String.format("User %d updated", id);
 	}
 
 	@DeleteMapping(path = "/{id}")
-	public void deleteUser(@PathVariable("id") Long id){
-		
+	public String deleteUser(@PathVariable("id") Long id){
+		this.userService.deleteUser(id);
+		return String.format("User %d deleted", id);
 	}	
 
 }
