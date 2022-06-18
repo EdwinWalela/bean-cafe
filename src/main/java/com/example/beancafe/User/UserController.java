daaa96b623f295f98d3a026b3274bf6411e03865
edwinwalela <edwinwalela@gmail.com>
2022-06-18 17:54:46 +0300

    user controller

--- src/main/java/com/example/beancafe/User/UserController.java
+++ src/main/java/com/example/beancafe/User/UserController.java
@@ -1,4 +1,41 @@
 package com.example.beancafe.User;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping(path = "/api/v1/users")
 public class UserController {
-  
+
+	@GetMapping()
+	public void getUsers(){
+
+	}
+	@GetMapping(path = "/{id}")
+	public void getUser(@PathVariable("id") Long id){
+
+	}
+
+	@PostMapping()
+	public void createUser(@RequestBody User user){
+
+	}
+
+	@PutMapping(path = "/{id}")
+	public void updateUser(@RequestBody User user, @PathVariable("id") Long id){
+
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public void deleteUser(@PathVariable("id") Long id){
+		
+	}	
+
 }
