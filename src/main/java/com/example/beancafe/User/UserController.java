ac335ac56aa06f9aecf5f40db5d98d2d24acf99c
edwinwalela <edwinwalela@gmail.com>
2022-06-19 11:09:45 +0300

    get users custom handler

--- src/main/java/com/example/beancafe/User/UserController.java
+++ src/main/java/com/example/beancafe/User/UserController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.NoSuchMessageException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
